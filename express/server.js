e619deb934855560cc9c93b045d8e8869e3f7baa
Мирзин Артур Робертович <artur.mirzin@akbarsdigital.ru>
2020-07-11 05:11:49 +0300

    Добавляет код для отправки сообщений в телеграм

--- express/server.js
+++ express/server.js
@@ -1,22 +1,45 @@
 'use strict';
-const express = require('express');
+require('dotenv').config({
+  path: '.env'
+});
 const path = require('path');
 const serverless = require('serverless-http');
+
+const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+const Telegram = require('telegraf/telegram');
+const telegram = new Telegram(process.env.BOT_TOKEN, {
+    agent: null,
+    webhookReply: true,
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+const Telegraf = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.use((ctx) => {
+    telegram.sendMessage(ctx.from.id, `Your Telegram id: ${ctx.from.id}`);
+});
+bot.startPolling();
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    next();
+  });
+
+  app.use(express.json());
+
+  app.get('/', (req, res) => {
+    telegram.sendMessage(
+      process.env.ARTHUR_ID,
+      `Hello, bro!`,
+    );
+    res.send('ok')
+  });
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
