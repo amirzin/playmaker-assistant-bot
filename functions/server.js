84b2ca455443ae3af60dcbba1a4685b1c28e0b28
Мирзин Артур Робертович <artur.mirzin@akbarsdigital.ru>
2020-07-11 06:04:03 +0300

    Избавляется от env переменных

--- functions/server.js
+++ functions/server.js
@@ -1,43 +1,42 @@
-'use strict';
-require('dotenv').config({
-  path: '.env'
+"use strict";
+require("dotenv").config({
+  path: ".env",
 });
-const path = require('path');
-const serverless = require('serverless-http');
+const path = require("path");
+const serverless = require("serverless-http");
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const Telegram = require('telegraf/telegram');
-const telegram = new Telegram(process.env.BOT_TOKEN, {
-    agent: null,
-    webhookReply: true,
+const Telegram = require("telegraf/telegram");
+const BOT_TOKEN = "1336920146:AAF9HoiPQl42WnQ3DAlDnDyO-CdTuws6ncg";
+const telegram = new Telegram(BOT_TOKEN, {
+  agent: null,
+  webhookReply: true,
 });
-const Telegraf = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const Telegraf = require("telegraf");
+const bot = new Telegraf(BOT_TOKEN);
 bot.use((ctx) => {
-    telegram.sendMessage(ctx.from.id, `Your Telegram id: ${ctx.from.id}`);
+  telegram.sendMessage(ctx.from.id, `Your Telegram id: ${ctx.from.id}`);
 });
 bot.startPolling();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-  app.use(express.json());
+app.use(express.json());
 
-  app.get('/', (req, res) => {
-    telegram.sendMessage(
-      process.env.ARTHUR_ID,
-      `Hello, bro!`,
-    );
-    res.send('ok')
-  });
+const receiverId = '19263838';
 
+app.get("/", (req, res) => {
+  telegram.sendMessage(receiverId, `Hello, bro!`);
+  res.send("ok");
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
