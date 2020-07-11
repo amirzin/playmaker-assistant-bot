c8850fa97f5c48b45fe7b3e7c56ef27423503bf1
Мирзин Артур Робертович <artur.mirzin@akbarsdigital.ru>
2020-07-11 06:13:35 +0300

    Пробую задеплоить

--- functions/server.js
+++ functions/server.js
@@ -1,42 +1,22 @@
-"use strict";
-require("dotenv").config({
-  path: ".env",
-});
-const path = require("path");
-const serverless = require("serverless-http");
-
-const express = require("express");
+'use strict';
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
 const app = express();
-const Telegram = require("telegraf/telegram");
-const BOT_TOKEN = "1336920146:AAF9HoiPQl42WnQ3DAlDnDyO-CdTuws6ncg";
-const telegram = new Telegram(BOT_TOKEN, {
-  agent: null,
-  webhookReply: true,
-});
-const Telegraf = require("telegraf");
-const bot = new Telegraf(BOT_TOKEN);
-bot.use((ctx) => {
-  telegram.sendMessage(ctx.from.id, `Your Telegram id: ${ctx.from.id}`);
-});
-bot.startPolling();
+const bodyParser = require('body-parser');
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
 });
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(express.json());
-
-const receiverId = '19263838';
-
-app.get("/", (req, res) => {
-  telegram.sendMessage(receiverId, `Hello, bro!`);
-  res.send("ok");
-});
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
