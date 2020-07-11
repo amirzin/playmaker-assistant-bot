302bc6e16c8608abef4b7b8c612eca3d75d2e477
Мирзин Артур Робертович <artur.mirzin@akbarsdigital.ru>
2020-07-11 06:19:30 +0300

    Пробую задеплоить

--- functions/server.js
+++ functions/server.js
@@ -1,22 +1,55 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+"use strict";
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+
+require("dotenv").config({
+  path: ".env",
+});
+const Telegram = require("telegraf/telegram");
+const BOT_TOKEN = "1336920146:AAF9HoiPQl42WnQ3DAlDnDyO-CdTuws6ncg";
+const telegram = new Telegram(BOT_TOKEN, {
+  agent: null,
+  webhookReply: true,
+});
+const Telegraf = require("telegraf");
+const bot = new Telegraf(BOT_TOKEN);
+bot.use((ctx) => {
+  telegram.sendMessage(ctx.from.id, `Your Telegram id: ${ctx.from.id}`);
+});
+bot.startPolling();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(express.json());
+
+const receiverId = "19263838";
+
+router.get("/", (req, res) => {
+  telegram.sendMessage(receiverId, `Hello, bro!`);
+  res.send("ok");
+
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
